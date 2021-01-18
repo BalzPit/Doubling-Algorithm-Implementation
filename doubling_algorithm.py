95c43e767628e6fa2dbbf7433112971433d2442f
BalzPit <44201522+BalzPit@users.noreply.github.com>
2021-01-18 18:18:51 +0100

    Comments and polish

--- doubling_algorithm.py
+++ doubling_algorithm.py
@@ -12,43 +12,46 @@ from scipy.spatial import distance
 from numpy.random import default_rng
 import linecache
 
-N = 5000
-k = 15
+input_path = "dataset.txt" #path of input file
+N = 5000 # number of input points
+k = 15  #number of clusters
 alpha = 2
 beta = 2
+
+#lists of all points read from input file
 pointsx = []
 pointsy = []
-#ranges = []
+
+#lists of cluster centers coordinates
 clustersx = []
 clustersy = []
 
-data = open("dataset.txt")
+#give a random order to the input every time the program is run
 line_index = 0
 rng = default_rng()
 random_lines = rng.choice(N, size=N, replace=False)
+
+
 
 def euclid_distance(p1, p2):
     return distance.euclidean(p1,p2)
 
 
 
-#compute clunion of close clusters
+#union of close clusters
 def merging_stage(di):
     global clustersx
     global clustersy
-    #global ranges
     
-    #merge clusters based on distance
+    #last phase centers
     temp_clustx = list(clustersx)
     temp_clusty = list(clustersy)
-    #temp_ranges = list(ranges)
     
     #remove every old center from lists
     clustersx = []
     clustersy = []
-    #ranges = []
     
-    #while that list is not empty
+    #while that list is not empty aka we considered every center
     while temp_clustx:
         num = len(temp_clustx)
         index = random.randint(0,num-1)
@@ -56,14 +59,10 @@ def merging_stage(di):
         #we are considering the indexed cluster
         centerx = temp_clustx.pop(index)
         centery = temp_clusty.pop(index)
-        #rng = temp_ranges.pop(index)
         
-        
-        #ranges.append(rng)
-        
+        #save all other centers to make comparison with indexed center easier
         temp2_clustx = list(temp_clustx)
         temp2_clusty = list(temp_clusty)
-        #temp2_ranges = list(temp_ranges)
         
         n = len(temp2_clustx)
         #find close centers
@@ -75,23 +74,12 @@ def merging_stage(di):
                 #this center is too close, its cluster is merged
                 temp_clustx.remove(temp2_clustx[i])
                 temp_clusty.remove(temp2_clusty[i])
-                #tr = temp2_ranges[i]
-                #temp_ranges.remove(tr)
-                """
-                #recalculate the range if necessary
-                tr += dist
-                if rng < tr:
-                    rng = tr
-                    #bound on the range
-                    if (rng > alpha*di):
-                        rng = alpha*di
-                        print("not epic")
-                        """
+
         clustersx.append(centerx)
         clustersy.append(centery)
-        #ranges.append(rng)
     
     return len(clustersx)
+
 
 
 #add one new point
@@ -102,21 +90,21 @@ def update_stage():
     global pointsx
     global pointsy
 
-    #generate new point
-    # Get next line from file 
+    # get next point from file 
     line = read_line() 
   
     # if line is empty, end of file is reached 
     if not line: 
         return len(clustersx)
 
-    #get point from data
+    #get point from data and separate coordinates
     line = line.strip()
     point = line.split()
     
     pointx = float(point[0])
     pointy = float(point[1])
 
+    #add new point to list of all points
     pointsx.append(pointx)
     pointsy.append(pointy)
 
@@ -125,22 +113,24 @@ def update_stage():
     #compare distance with every cluster center
     for i in range(0,numclusters):
         if euclid_distance((pointx,pointy),(clustersx[i],clustersy[i])) < d*alpha:#ranges[i] :
-            #the point is within some cluster
+            #the point is within some cluster, nothing happens
             check = 0
     
     if check == 1:
-        #the point was away from all clusters, create a new cluster
+        #the point was far enough from all clusters, create a new cluster
         clustersx.append(pointx)
         clustersy.append(pointy)
-#        ranges.append(d)
         
     return len(clustersx)
 
+
+
+#read next random line from input file
 def read_line():
     global line_index
     global random_lines
     
-    line = linecache.getline("dataset.txt", random_lines[line_index])
+    line = linecache.getline(input_path, random_lines[line_index])
     
     if line_index < N-1:
         line_index += 1
@@ -149,6 +139,8 @@ def read_line():
 
 #==============================================================================
 
+
+#--------------------------------------------------INITIALIZATION
 count = 0
 while count < k+1 : 
     count += 1
@@ -172,54 +164,43 @@ while count < k+1 :
 plt.scatter(pointsx,pointsy)
 plt.show()
 
-
-for i in range(0,k+1):
-    ranges.append(0)
-
 #find minimum distance
-d = 500000
-#rangei = 0
-#rangej = 0
-
+d = 5000000
 for i in range(0,k+1):
     for j in range(0,k+1):
         dist = euclid_distance((pointsx[i], pointsy[i]), (pointsx[j], pointsy[j]))
         if (dist<d and i != j):
             d = dist
-  #          rangei = i
- #           rangej = j
+            
 print(d)
 
-#associate minimum range with correct cluster indexes
-#ranges[rangei] = d
-#ranges[rangej] = d
-
-#for i in range(0,k+1):
-#    ranges[i] = d
-
+#initilaise cluster centers lists
 clustersx = list(pointsx)
 clustersy = list(pointsy)
 
+
 i = 0
+
+#start collecting new points
+
 while i<N:
+    #update d
     d= beta*d
     print(d)
     
-    # MERGING stage
-    print("MERGING STAGE")
-    
+    # --------------------------------------------MERGING stage
     m = merging_stage(d)
     
     plt.scatter(clustersx, clustersy, color = 'red')
     plt.show()
     
     print("UPDATE STAGE")
-    #accept new inputs and UPDATE
+    #accept new inputs and -----------------------UPDATE
     while m<k+1 and i<N:
         m = update_stage()
         i+=1
     
-    #draw points
+    #draw points and centers after end of phase
     f, ax = plt.subplots(1)
     ax.scatter(pointsx, pointsy ,color = "blue")
     ax.scatter(clustersx, clustersy ,color = "red")
@@ -229,11 +210,9 @@ while i<N:
 
     plt.show(f)
 
-#draw final points
+#draw final points and centers
 f, ax = plt.subplots(1)
 ax.scatter(pointsx, pointsy ,color = "blue")
 ax.scatter(clustersx, clustersy ,color = "red")
 
 plt.show(f)
-
-data.close()
